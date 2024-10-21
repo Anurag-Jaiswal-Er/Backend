dc484acd64924c5629848e6f980e4bb3e22685c2
Anurag Jaiswal <134679771+Anurag-Jjaiswal@users.noreply.github.com>
2024-10-21 19:32:26 +0530

    utils code disscussion

---
+++ you_tube_backend/src/utils/asyncHandler.js
@@ -0,0 +1,22 @@
+//  promises wala part
+
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(res, req, next).catch((error) => next(error));
+  };
+};
+
+export { asyncHandler };
+
+// try catch wal part
+// const asyncHandler = (fn) => async (req, res, next) => {
+//   try {
+//     await fn(req, res, next);
+//   } catch (error) {
+//     res.status(err.code || 500).json({
+//       success: false,
+//       message: err.message,
+//     });
+//     console.log();
+//   }
+// };
